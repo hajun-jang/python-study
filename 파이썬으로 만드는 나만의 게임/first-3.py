c1dab14974c7d3bf8eb765aef0345420306e63de
hajun-jang <hajun.jang2013@gmail.com>
2025-05-11 08:56:48 +0900

    UI edit

--- 파이썬으로 만드는 나만의 게임/first-3.py
+++ 파이썬으로 만드는 나만의 게임/first-3.py
@@ -135,11 +135,11 @@ print(now)
 year = str(now.tm_year)
 month = str(now.tm_mon)
 day = str(now.tm_mday)
-print(year + "년" + month + "월" + day + "일")
+print(year + "년 " + month + "월 " + day + "일 ")
 hour = str(now.tm_hour)
 minute = str(now.tm_min)
 sec = str(now.tm_sec)
-print(hour + "시" + minute + "분" + sec + "초")
+print(hour + "시 " + minute + "분 " + sec + "초 ")
 print("-"*100)  #칸 나누기
 ###################################################
 #2.6.2
